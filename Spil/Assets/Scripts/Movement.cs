f926341cc95d1584ecb2dd2244e9528ad49dede8
DESKTOP-PQIDIEU\UNFGRUPPE5PC1 <Prog@gr5.gdc>
2018-07-27 22:13:38 +0200

    Kodning, vand og baggrund

---
+++ Spil/Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float moveSpeed;
+    public float rotationSpeed;
+    public string playerControlForwardBackwards;
+    public string playerControlLeftRight;
+    public Rigidbody Submarine;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 0, -rotationSpeed * Input.GetAxis(playerControlForwardBackwards) * Time.deltaTime, Space.Self);
+    }
+
+    private void FixedUpdate()
+    {
+        Move(moveSpeed * Input.GetAxis(playerControlLeftRight));
+    }
+
+    public void Move(float speed) {
+        Submarine.velocity = (Vector3.up * Submarine.velocity.y) + (transform.up * speed);
+    }
+
+}
