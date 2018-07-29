fb8280d2993654166215afe130f86eb4116156d9
DESKTOP-PQIDIEU\UNFGRUPPE5PC1 <Prog@gr5.gdc>
2018-07-30 01:12:31 +0200

    Everything WORKS!!!

---
+++ Spil/Assets/Scripts/Countdown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour {
+
+
+    public Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+
+        dive();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void dive()
+    {
+        //anim.SetFloat("Base", 1f);
+    }
+
+}
