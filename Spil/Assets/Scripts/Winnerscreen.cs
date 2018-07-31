5c81c58662e1eed5c1fb1e3b39b10376e1c6f03b
DESKTOP-PQIDIEU\UNFGRUPPE5PC1 <Prog@gr5.gdc>
2018-07-31 11:01:41 +0200

    WinnerScreen transition lavet

---
+++ Spil/Assets/Scripts/Winnerscreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Winnerscreen : MonoBehaviour {
+
+    //public float yellowWinner;
+    //public float redWinner;
+    public FindWinner winner;
+    public GameObject Yellow;
+    public GameObject Red;
+
+	// Use this for initialization
+	void Start () {
+
+        winner = GameObject.FindObjectOfType<FindWinner>();
+
+        if (winner.winner == "Player1")
+        {
+            Yellow.SetActive(false);
+            Red.SetActive(true);
+        }
+        else if (winner.winner == "Player2")
+        {
+            Red.SetActive(false);
+            Yellow.SetActive(true);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
